8a155a7e4baa9129a34e07cdeba73bc76c17ab4c
Adi Skop <adiskop@gmail.com>
2020-06-18 08:13:23 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,6 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-converted_input= "user_input".to_i
-converted_input=gets.strip
+user_input.to_i
 
 end
-input_to_index(3)
